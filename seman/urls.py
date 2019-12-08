a8621209ee1d856bcc56378fe4e9d9bf11892b20
Константин <kostyarach40@gmail.com>
2019-12-09 01:09:41 +0300

    create read docx file

--- seman/urls.py
+++ seman/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('upload_file/', views.upload_file, name='upload_file'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
